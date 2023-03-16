8265ce565540685c2c82b23d22fe46408c16e077
GamoraKrut <arturgamurar1488@gmail.com>
2023-03-16 21:58:53 +0300

    poreshal zadachi

---
+++ Zadachi.py
@@ -0,0 +1,37 @@
+# a = float(input())  #Нахождение площади треугольника
+# b = float(input())
+# square = 0.5 * a * b
+# print(square)
+
+# s = float(input())  #Нахождение времени до встречи двух объектов
+# v1 = float(input())
+# v2 = float(input())
+# t = s / (v1 + v2)
+# print(t)
+
+# number = float(input())  #Нахождение обратного числа
+# if number != 0:
+#     print(number ** -1)
+# else:
+#     print('Обратного числа не существует')
+
+# fahrengheit = float(input())  # онвертация температуры из Фаренгейт в Цельсий
+# celsium = (5/9)*(fahrengheit - 32)
+# print(celsium)
+
+# dog_age = int(input())  #Программа, которая вычисляет возраст собаки в человеческих годах
+# if 0 < dog_age < 3:
+#     print(dog_age * 10.5)
+# else:
+#     print(21 + (dog_age - 2) * 4)
+
+# number = float(input())  # Вывести первую цифру после запятой
+# first_number = number * 10 % 10
+# first_number = int(first_number)
+# print(first_number)
+
+# number = float(input())  # Вывод его дробной части
+# last_numbers = ((number * 10) % 10) / 10
+# print(last_numbers)
+
+#
